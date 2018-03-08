f53494629dde0b71ef5feb04c116faba48eac0a6
Blair Learn <blairlearn@users.noreply.github.com>
2018-03-08 13:54:56 -0500

    Fix send for multiple email recipients
    Changes by CBIIT engineer. Splits the list of recipients into an array,
    allowing for greater tolerance of formatting errors in the list.
    Also cleans up some trailing whitespace.

--- aws_refresh.ps1
+++ aws_refresh.ps1
@@ -7,7 +7,7 @@ function DBRefresh($settings) {
 
   $command.Parameters.Add("@ReturnValue", [System.Data.SqlDbType]"Int")  | Out-Null
   $command.Parameters["@ReturnValue"].Direction = [System.Data.ParameterDirection]"ReturnValue"
-    
+
   try {
         $connection.Open()  | out-null
         $command.ExecuteNonQuery()  | out-null
@@ -18,90 +18,90 @@ function DBRefresh($settings) {
     }
 
 return $returnValue
-}   
+}
 
 
 function Main() {
-    try {
-            
+  try {
             [xml]$settings = Get-Content "settings.xml"
+            $LocalTo = $settings.settings.email.to.split(",")
 
             if ( $settings.settings.testmode -eq 1 ) { write-host 'start copy'}
-            # Copy file goes here 
-            
-            cmd /c $settings.settings.file.batchcmd |Out-String  
-            
+            # Copy file goes here
+
+            cmd /c $settings.settings.file.batchcmd |Out-String
+
             $status = $LastExitCode
-            
+
            write-host "Copy file exit code: " $status
-           
-           If ( $status -ne 0 ) { 
-           
+
+           If ( $status -ne 0 ) {
+
            send-mailmessage `
                                 -SmtpServer $settings.settings.email.server `
                                 -From $settings.settings.email.from `
-                                -To $settings.settings.email.to `
+                                -To $LocalTo `
                                 -Subject $settings.settings.email.subjectLine `
                                 -BODY 'copy file failed'
-           
+
            return 1}
 
-            if ( $settings.settings.testmode -eq 1 ) { write-host 'start DB'} 
+
+            if ( $settings.settings.testmode -eq 1 ) { write-host 'start DB'}
 
             $status=DBRefresh($settings)
 
             if ( $settings.settings.testmode -eq 1 ) { write-host 'DB restore return code: ' $status }
-            
-            If ( $status -ne 0 ) { 
-           
+
+            If ( $status -ne 0 ) {
+
            send-mailmessage `
                                 -SmtpServer $settings.settings.email.server `
                                 -From $settings.settings.email.from `
-                                -To $settings.settings.email.to `
+                                -To $LocalTo `
                                 -Subject $settings.settings.email.subjectLine `
                                 -BODY 'DB restore failed'
-           
+
            return 1}
-            
-            
-       
-            if ( $settings.settings.testmode -eq 1 ) { 
-            $plinkcommand = "plink.exe $($settings.settings.ES.server) -i $($settings.settings.ES.privatekey) -l $($settings.settings.ES.userid) ls "
+
+
+            if ( $settings.settings.testmode -eq 1 ) {
+             $plinkcommand = ".\plink.exe $($settings.settings.ES.server) -l $($settings.settings.ES.userid) -pw $($settings.settings.ES.password) ls "
             write-host 'Start ES. here is plink': $plinkcommand
-            }  else {         
-                 $plinkcommand = "plink.exe $(settings.settings.ES.server) -i $($settings.settings.ES.privatekey) -l $($settings.settings.ES.userid) /home/elasticsearch/tools/aws_restore.sh"
+            }  else {
+                 $plinkcommand = ".\plink.exe $($settings.settings.ES.server) -l $($settings.settings.ES.userid) -pw $($settings.settings.ES.password) /home/elasticsearch/tools/aws_restore.sh "
                  }
-            
+
             $msg = Invoke-Expression $plinkcommand
             write-host 'ES index refresh message: ': $msg
-             if ( $msg -eq 'success' ) {   
+             if ( $msg -eq 'success' ) {
              write-host 'success'
               } else {
-                        $msg = 'ES failed: ' + $msg 
+                        $msg = 'ES failed: ' + $msg
                         send-mailmessage `
                                 -SmtpServer $settings.settings.email.server `
                                 -From $settings.settings.email.from `
-                                -To $settings.settings.email.to `
+                                -To $LocalTo `
                                 -Subject $settings.settings.email.subjectLine `
                                 -BODY $msg
-                           
+
                         return 1
                     }
 
         }
     catch [System.Exception] {
-    
+
                         $explanationMessage = "Error occured. `n$_`n`n`nError at line: " +
                         $_.InvocationInfo.ScriptLineNumber + "`n" +
                         $_.InvocationInfo.line
-                        write-host $explanationMessage 
+                        write-host $explanationMessage
                         send-mailmessage `
                                 -SmtpServer $settings.settings.email.server `
                                 -From $settings.settings.email.from `
-                                -To $settings.settings.email.to `
+                                -To $LocalTo `
                                 -Subject $settings.settings.email.subjectLine `
                                 -BODY $explanationMessage
-                        
+
     }
 }
 
@@ -113,7 +113,4 @@ Main
 
 
 
-
-
- 
 
